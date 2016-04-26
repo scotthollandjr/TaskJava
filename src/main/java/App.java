a83a73210d5838e86b29ab63e1c84938d403b3b4
DJ Roychoudhury and Scott McIntire <dj@>
2016-04-26 08:48:35 -0700

    pass rootTest for first integration test.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,30 +1,30 @@
-// import java.util.*;
-// import java.util.Map;
-// import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
-//
-// public class App {
-//   public static void main(String[] args) {
-//     staticFileLocation("/public");
-//
-//     get("/", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//       model.put("template", "templates/home.vtl");
-//       return new ModelAndView(model, "templates/layout.vtl");
-//     }, new VelocityTemplateEngine());
-//
-//     get("/detector", (request, response) -> {
-//       Map<String, Object> model = new HashMap<String, Object>();
-//
-//       String userInput = request.queryParams("formInputName");
-//       App newApp = new App();
-//       String varName = newApp.methodName(userInput);
-//       model.put("varName", varName);
-//
-//       model.put("template", "templates/detector.vtl");
-//       return new ModelAndView(model, "templates/layout.vtl");
-//     }, new VelocityTemplateEngine());
-//   }
-// }
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // get("/detector", (request, response) -> {
+    //   Map<String, Object> model = new HashMap<String, Object>();
+    //
+    //   String userInput = request.queryParams("formInputName");
+    //   App newApp = new App();
+    //   String varName = newApp.methodName(userInput);
+    //   model.put("varName", varName);
+    //
+    //   model.put("template", "templates/detector.vtl");
+    //   return new ModelAndView(model, "templates/layout.vtl");
+    // }, new VelocityTemplateEngine());
+  }
+}
