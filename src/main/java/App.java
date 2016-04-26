29d3753a00c1a2c02d3070c7c5b77e9da37571cc
DJ Roychoudhury and Scott McIntire <scott@>
2016-04-26 08:32:20 -0700

    create intial files, instantiate test pass.

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+// import java.util.*;
+// import java.util.Map;
+// import java.util.HashMap;
+// import spark.ModelAndView;
+// import spark.template.velocity.VelocityTemplateEngine;
+// import static spark.Spark.*;
+//
+// public class App {
+//   public static void main(String[] args) {
+//     staticFileLocation("/public");
+//
+//     get("/", (request, response) -> {
+//       Map<String, Object> model = new HashMap<String, Object>();
+//       model.put("template", "templates/home.vtl");
+//       return new ModelAndView(model, "templates/layout.vtl");
+//     }, new VelocityTemplateEngine());
+//
+//     get("/detector", (request, response) -> {
+//       Map<String, Object> model = new HashMap<String, Object>();
+//
+//       String userInput = request.queryParams("formInputName");
+//       App newApp = new App();
+//       String varName = newApp.methodName(userInput);
+//       model.put("varName", varName);
+//
+//       model.put("template", "templates/detector.vtl");
+//       return new ModelAndView(model, "templates/layout.vtl");
+//     }, new VelocityTemplateEngine());
+//   }
+// }
