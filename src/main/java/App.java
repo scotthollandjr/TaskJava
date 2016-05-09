26d194b67700aa0425290c808805253fcf24cd6e
Scott Holland <Scotthollandjr@gmail.com>
2016-05-09 09:34:27 -0700

    Add front end with passing tests

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,89 +1,87 @@
-import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.ArrayList;
 
 public class App {
-
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-      model.put("tasks", request.session().attribute("tasks"));
+      HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // get("tasks/new", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/task-form.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/tasks", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("tasks", Task.all());
-    //   model.put("template", "templates/tasks.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/tasks", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
-    //   String description = request.queryParams("description");
-    //   Task newTask = new Task(description);
-    //   category.addTask(newTask);
-    //   model.put("category", category);
-    //   model.put("template", "templates/category.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/tasks/:id", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   Task task = Task.find(Integer.parseInt(request.params(":id")));
-    //   model.put("task", task);
-    //   model.put("template", "templates/task.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/categories", (request, response) -> {
-    //     HashMap<String, Object> model = new HashMap<String, Object>();
-    //     model.put("categories", Category.all());
-    //     model.put("template", "templates/categories.vtl");
-    //     return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/categories/new", (request, response) -> {
-    //     HashMap<String, Object> model = new HashMap<String, Object>();
-    //     model.put("template", "templates/category-form.vtl");
-    //     return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // post("/categories", (request, response) -> {
-    //     HashMap<String, Object> model = new HashMap<String, Object>();
-    //     String name = request.queryParams("name");
-    //     Category newCategory = new Category(name);
-    //     model.put("category", newCategory);
-    //     model.put("template", "templates/success.vtl");
-    //     return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/categories/:id", (request, response) -> {
-    //     HashMap<String, Object> model = new HashMap<String, Object>();
-    //     model.put("categories", Category.find(Integer.parseInt(request.params(":id"))));
-    //     model.put("template", "templates/category.vtl");
-    //     return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/categories/:id/tasks/new", (request, response) -> {
-    //     HashMap<String, Object> model = new HashMap<String, Object>();
-    //     model.put("categories", Category.find(Integer.parseInt(request.params(":id"))));
-    //     model.put("template", "templates/category-tasks-form.vtl");
-    //     return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    get("/categories", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("categories", Category.all());
+      model.put("template", "templates/categories.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/tasks", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String description = request.queryParams("description");
+      Task newTask = new Task(description);
+      newTask.save();
+      response.redirect("/tasks");
+      return null;
+    });
+
+    post("/categories", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Category newCategory = new Category(name);
+      newCategory.save();
+      response.redirect("/categories");
+      return null;
+    });
+
+    get("/tasks/:id", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params("id")));
+      model.put("task", task);
+      model.put("allCategories", Category.all());
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/:id", (request,response) ->{
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params("id")));
+      model.put("category", category);
+      model.put("allTasks", Task.all());
+      model.put("template", "templates/category.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/add_tasks", (request, response) -> {
+      int taskId = Integer.parseInt(request.queryParams("task_id"));
+      int categoryId = Integer.parseInt(request.queryParams("category_id"));
+      Category category = Category.find(categoryId);
+      Task task = Task.find(taskId);
+      category.addTask(task);
+      response.redirect("/categories/" + categoryId);
+      return null;
+    });
+
+    post("/add_categories", (request, response) -> {
+      int taskId = Integer.parseInt(request.queryParams("task_id"));
+      int categoryId = Integer.parseInt(request.queryParams("category_id"));
+      Category category = Category.find(categoryId);
+      Task task = Task.find(taskId);
+      task.addCategory(category);
+      response.redirect("/tasks/" + taskId);
+      return null;
+    });
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,28 +1,16 @@
+import org.sql2o.*;
+import org.junit.*;
 import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
-import org.junit.*;
-import java.util.List;
-import org.sql2o.*;
+import static org.junit.Assert.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
-  }
-
-  @After
-  public void tearDown() {
-    try(Connection con = DB.sql2o.open()) {
-      String deleteTasksQuery = "DELETE FROM tasks *;";
-      String deleteCategoriesQuery = "DELETE FROM categories *;";
-      con.createQuery(deleteTasksQuery).executeUpdate();
-      con.createQuery(deleteCategoriesQuery).executeUpdate();
-    }
-  }
 
   @Override
   public WebDriver getDefaultDriver() {
@@ -32,51 +20,77 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("To-do List");
+    assertThat(pageSource()).contains("Todo list!");
   }
-  // 
-  // @Test
-  // public void categoryIsCreatedTest() {
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Add a New Category"));
-  //   fill("#name").with("Household chores");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("Your category has been saved.");
-  // }
-  //
-  // @Test
-  // public void categoryIsDisplayedTest() {
-  //   goTo("http://localhost:4567/categories/new");
-  //   fill("#name").with("Household chores");
-  //   submit(".btn");
-  //   click("a", withText("View categories"));
-  //   assertThat(pageSource()).contains("Household chores");
-  // }
-  //
-  //   @Test
-  // public void categoryTasksFormIsDisplayed() {
-  //   goTo("http://localhost:4567/categories/new");
-  //   fill("#name").with("Shopping");
-  //   submit(".btn");
-  //   click("a", withText("View categories"));
-  //   click("a", withText("Shopping"));
-  //   click("a", withText("Add a new task"));
-  //   assertThat(pageSource()).contains("Add a Task to Shopping");
-  // }
-  //
-  // @Test
-  // public void tasksIsAddedAndDisplayed() {
-  //   goTo("http://localhost:4567/categories/new");
-  //   fill("#name").with("Banking");
-  //   submit(".btn");
-  //   click("a", withText("View categories"));
-  //   click("a", withText("Banking"));
-  //   click("a", withText("Add a new task"));
-  //   fill("#description").with("Deposit paycheck");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("Deposit paycheck");
-  // }
+
+  @Test
+  public void categoryIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Categories"));
+    fill("#name").with("Household chores");
+    submit(".btn");
+    assertThat(pageSource()).contains("Household chores");
+  }
+
+  @Test
+  public void taskIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Tasks"));
+    fill("#description").with("Mow the lawn");
+    submit(".btn");
+    assertThat(pageSource()).contains("Mow the lawn");
+  }
+
+  @Test
+  public void categoryShowPageDisplaysName() {
+    Category testCategory = new Category("Household chores");
+    testCategory.save();
+    String url = String.format("http://localhost:4567/categories/%d", testCategory.getId());
+    goTo(url);
+    assertThat(pageSource()).contains("Household chores");
+  }
+
+  @Test
+  public void taskShowPageDisplaysDescription() {
+    Task testTask = new Task("Mow the lawn");
+    testTask.save();
+    String url = String.format("http://localhost:4567/tasks/%d", testTask.getId());
+    goTo(url);
+    assertThat(pageSource()).contains("Mow the lawn");
+  }
+
+  @Test
+  public void taskIsAddedToCategory() {
+    Category testCategory = new Category("Household chores");
+    testCategory.save();
+    Task testTask = new Task("Mow the lawn");
+    testTask.save();
+    String url = String.format("http://localhost:4567/categories/%d", testCategory.getId());
+    goTo(url);
+    fillSelect("#task_id").withText("Mow the lawn");
+    submit(".btn");
+    assertThat(pageSource()).contains("<li>");
+    assertThat(pageSource()).contains("Mow the lawn");
+  }
+
+  @Test
+  public void categoryIsAddedToTask() {
+    Category testCategory = new Category("Household chores");
+    testCategory.save();
+    Task testTask = new Task("Mow the lawn");
+    testTask.save();
+    String url = String.format("http://localhost:4567/tasks/%d", testTask.getId());
+    goTo(url);
+    fillSelect("#category_id").withText("Household chores");
+    submit(".btn");
+    assertThat(pageSource()).contains("<li>");
+    assertThat(pageSource()).contains("Household chores");
+  }
+
 }
